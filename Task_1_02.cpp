967a4f0b5e1f808792c09c335f4380088ecc9244
spzkv-vl <125677619+spzkv-vl@users.noreply.github.com>
2023-03-01 19:33:44 +0800

    Create Task_1_02.cpp

---
+++ Task_1_02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int num = 39;
+    int f = 4;
+    float c = num/f;
+    int l = num % f;
+    cout << c << '\n';
+    cout << l << '\n';
+return 0;
+}
