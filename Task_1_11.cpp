a35ed9043cf6418a7777e5a5f93b2b8c2f610e8b
spzkv-vl <125677619+spzkv-vl@users.noreply.github.com>
2023-03-05 14:02:09 +0800

    Create Task_1_11.cpp

---
+++ Task_1_11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, d;
+    d=3;
+    for (n=0; n<=100; n++) {
+    if (n%d==0) {
+    cout << n << "   ";
+    }
+    }
+return 0;
+}
