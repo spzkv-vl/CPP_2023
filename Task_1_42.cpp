834e3e3ce6093303c33f5d219c7da2363b5e85eb
spzkv-vl <125677619+spzkv-vl@users.noreply.github.com>
2023-03-05 13:56:57 +0800

    Create Task_1_42.cpp

---
+++ Task_1_42.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i, j, p, a[10]={1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+    for (j=0; j<=10; j++) {
+        for (i=0; i<=(10-j); i++) {
+            if (a[i]<a[i+1]) {
+                p=a[i];
+                a[i]=a[i+1];
+                a[i+1]=p;
+            }
+        }
+    }
+    for (i = 1; i <= 10; i++)
+    {
+        cout << a[i] << "   ";
+    }
+return 0;
+}
