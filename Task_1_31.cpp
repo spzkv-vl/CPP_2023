cc4fc146251c6e92220616a3bf93b6027bd89f69
spzkv-vl <125677619+spzkv-vl@users.noreply.github.com>
2023-03-09 21:40:10 +0800

    Create Task_1_31.cpp

---
+++ Task_1_31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a[10]={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    cout << a[n] << "   ";
+   return 0;
+}
