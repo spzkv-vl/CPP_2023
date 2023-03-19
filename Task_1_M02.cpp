eb2d5bc7a25e9f40a9cbccfaf46427414a51579e
spzkv-vl <125677619+spzkv-vl@users.noreply.github.com>
2023-03-19 17:31:32 +0800

    Update Task_1_M02.cpp

--- Task_1_M02.cpp
+++ Task_1_M02.cpp
@@ -2,15 +2,14 @@
 #include <math.h>
 #include <iomanip>
 using namespace std;
-int Square(float A, int n){
+float Square(float A, int n){
 float R, S;
 double alfa, gamma, ThisIsAPi = 3.1415926535897932;
-alfa = 180/n*ThisIsAPi/180;
+alfa = 180/(float)n*ThisIsAPi/180;
 R=A/(2*sin(alfa));
-gamma=360/n*ThisIsAPi/180;
+gamma=360/(float)n*ThisIsAPi/180;
 S=0.5*R*R*n*sin(gamma);
-cout <<  S << '\n';
-return 0;
+return S;
 }
 int main(){
     float A, S;
@@ -20,4 +19,5 @@ cin >> A;
 cout << "Enter the number of sides of the N-gon (at least 3) n= ";
 cin >> n;
 S=Square(A,n);
+cout <<  S << '\n';
 }
